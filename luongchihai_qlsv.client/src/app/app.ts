a7dee5f711971dbea89b1dc09db40824ec8b5e57
Gural <122676712+GuralSyncalypse@users.noreply.github.com>
2026-06-24 11:18:33 +0700

    Revert "feat(student): Implement Fluent API and Data Transfer Objects (#6)"
    This reverts commit 4c0020cab3f1e32068cf94e53d2243bd3b067f31.

--- luongchihai_qlsv.client/src/app/app.ts
+++ luongchihai_qlsv.client/src/app/app.ts
@@ -1,5 +1,12 @@
-import { DatePipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, signal } from '@angular/core';
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,6 +15,24 @@ import { Component, OnInit, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App implements OnInit {
-  ngOnInit(): void {
+  public forecasts: WeatherForecast[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getForecasts();
   }
+
+  getForecasts() {
+    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
+      (result) => {
+        this.forecasts = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  protected readonly title = signal('luongchihai_qlsv.client');
 }
