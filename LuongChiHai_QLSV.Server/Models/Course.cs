24c6c54b521ab612a3e59c5420b7bcec231915e8
Gural <122676712+GuralSyncalypse@users.noreply.github.com>
2026-06-25 09:40:32 +0700

    feat(course) (#9)

--- LuongChiHai_QLSV.Server/Models/Course.cs
+++ LuongChiHai_QLSV.Server/Models/Course.cs
@@ -2,13 +2,12 @@
 
 namespace LuongChiHai_QLSV.Server.Models
 {
-    [Table("Course")]
     public class Course
     {
-        public int courseID { get; set; }
-        public string courseName { get; set; } = string.Empty;
-        public string courseCode { get; set; } = string.Empty;
-        public int? credits { get; set; } // Số tín chỉ
+        public int CourseID { get; set; }
+        public string CourseName { get; set; } = string.Empty;
+        public string CourseCode { get; set; } = string.Empty;
+        public int? Credits { get; set; } // Số tín chỉ
 
         // Navigation property (Mối quan hệ 1 - Nhiều với Enrollment)
         public virtual ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
